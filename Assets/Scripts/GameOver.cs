958faf733e3726bdd9253df77469ffe5fd554079
restubumiii <140098956+restubumiii@users.noreply.github.com>
2023-08-25 15:13:04 +0700

    Add files via upload

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+    float timer = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            Data.score = 0;
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
